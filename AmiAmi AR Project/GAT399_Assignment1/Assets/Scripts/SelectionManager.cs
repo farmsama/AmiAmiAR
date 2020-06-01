17010d81c7abbf8e5f4d484d6fb48f8850efeb9b
GoGitGood <54795662+GoGitGood@users.noreply.github.com>
2020-06-01 11:49:00 +0800

    Manager
    Press Q to load model when in SampleScene
    PressW to enter SampleScene2

---
+++ AmiAmi AR Project/GAT399_Assignment1/Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectionManager : MonoBehaviour
+{
+    public ModelData[] ListOfFigurines;
+    public static ModelData SelectedFigurine;
+
+
+    public void Update()
+    {
+        DebugTests();   
+    }
+
+    public void ClearSelected()
+    {
+        SelectedFigurine = null;
+    }
+
+    public void ReplaceSelected(ModelData _selected)
+    {
+        SelectedFigurine = _selected;
+    }
+
+    private void DebugTests()
+    {
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            ReplaceSelected(ListOfFigurines[0]);
+
+            if (SelectedFigurine == null) Debug.Log("is null");
+            else Debug.Log("is not null");
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            if (SelectedFigurine == null) Debug.Log("is null");
+            else Debug.Log("is not null");
+
+            SceneManager.LoadScene("SampleScene2");
+        }
+
+        if (SelectedFigurine == null) Debug.Log("Has nothing selected");
+    }
+}
