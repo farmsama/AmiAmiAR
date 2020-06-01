186446fbc8f8e705c6f3a14a911f0e7c0a64525c
GoGitGood <54795662+GoGitGood@users.noreply.github.com>
2020-06-01 10:43:05 +0800

    Added Scriptable

---
+++ AmiAmi AR Project/GAT399_Assignment1/Assets/Scripts/Scriptable/ModelData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Figurine", menuName = "Figurine")]
+public class ModelData : ScriptableObject
+{
+    public Transform position;
+    public GameObject prefab;
+}
