cbd698d5fcb541cf3c564aefe3668b32816a15a2
Kok Tai LIM <koktai.lim@digipen.edu>
2020-06-05 05:28:26 +0800

    GroundPlane Test 2
    - Included Mobile Phone Rotation Control (drag left/right)
    - Button to allow repositioning of Figure

---
+++ AmiAmi AR Project/GAT399_Assignment1/Assets/Scripts/PlaneSpawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class PlaneSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    AnchorInputListenerBehaviour AILB;
+    PlaneFinderBehaviour PFB;
+    ContentPositioningBehaviour CPB;
+
+    DataSet dataset;
+
+    public GameObject FigurePrefab;
+    public GameObject GroundPlaneStage;
+
+    GameObject spawnedFigure = null;
+
+    void Start()
+    {
+        AILB = GetComponent<AnchorInputListenerBehaviour>();
+        PFB = GetComponent<PlaneFinderBehaviour>();
+        CPB = GetComponent<ContentPositioningBehaviour>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //public void SpawnFigure()
+    //{
+    //    if (spawnedFigure == null)
+    //    {
+    //        spawnedFigure = Instantiate(FigurePrefab);
+    //        spawnedFigure.transform.SetParent(GroundPlaneStage.transform);
+    //        spawnedFigure.transform.localPosition = Vector3.zero;
+    //    }
+    //}
+
+    public void ResetFigure()
+    {
+        //for(int i = 0; i < GameObject.FindGameObjectsWithTag("Figurine").Length ; i++ )
+        //{
+        //    Destroy(GameObject.FindGameObjectsWithTag("Figurine")[i]); 
+        //}
+        
+                
+        AILB.enabled = true;
+    }
+
+    
+}
