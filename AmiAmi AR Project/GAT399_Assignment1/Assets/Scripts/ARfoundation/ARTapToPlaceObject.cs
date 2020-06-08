8cef89eb4088ed387aff0f2f153eec13320d24c9
GoGitGood <54795662+GoGitGood@users.noreply.github.com>
2020-06-08 11:55:30 +0800

    ARfoundation - scenes and scripts related to it

---
+++ AmiAmi AR Project/GAT399_Assignment1/Assets/Scripts/ARfoundation/ARTapToPlaceObject.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARSubsystems;
+using System;
+
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    public GameObject placementIndicator;
+    public Camera cam;
+
+    private ARSessionOrigin arOrigin;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+
+    void Start()
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+    }
+
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    void UpdatePlacementPose()
+    {
+        var screenCenter = cam.ViewportToScreenPoint(new Vector3(0.5f,0.5f));        
+        var hits = new List<ARRaycastHit>();
+        var rayCastMgr = GetComponent<ARRaycastManager>();
+        rayCastMgr.Raycast(screenCenter, hits, TrackableType.Planes);
+
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+
+            var CameraForward = cam.transform.forward;
+            var CamBearing = new Vector3(CameraForward.x, 0, CameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(CamBearing);
+        }
+    }
+}
