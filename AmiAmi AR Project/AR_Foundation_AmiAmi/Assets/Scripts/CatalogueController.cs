37ae185c74b86386fae508be74f1154fdef58fff
GoGitGood <54795662+GoGitGood@users.noreply.github.com>
2020-06-21 18:48:36 +0800

    Got the bars to open properly
    > can't seem to fix the scrolling to lock at the top and bottom

---
+++ AmiAmi AR Project/AR_Foundation_AmiAmi/Assets/Scripts/CatalogueController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatalogueController : MonoBehaviour
+{
+    public GameObject Segment1;
+    public GameObject Subgroup1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClickSegment1()
+    {
+        if (Subgroup1.activeSelf == false)
+            Subgroup1.SetActive(true);
+        else
+            Subgroup1.SetActive(false);
+
+        RectTransform RT_s1 = Segment1.GetComponent<RectTransform>();
+        if (RT_s1.sizeDelta.y == 400)
+            RT_s1.sizeDelta = new Vector2(RT_s1.sizeDelta.x, 200);
+        else if (RT_s1.sizeDelta.y == 200)
+            RT_s1.sizeDelta = new Vector2(RT_s1.sizeDelta.x, 400);
+    }
+}
