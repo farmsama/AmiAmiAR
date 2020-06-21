4c7682a69f2eb6fbbf520ebf8c76cbded9cc7014
Kok Tai LIM <koktai.lim@digipen.edu>
2020-06-21 20:58:32 +0800

    Finger Drag Gesture

---
+++ AmiAmi AR Project/AR_Foundation_AmiAmi/Assets/Scripts/AnimEventFunctions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimEventFunctions : MonoBehaviour
+{
+    public void DestroyParent()
+    {
+        Destroy(gameObject.transform.parent.gameObject);
+    }
+
+}
