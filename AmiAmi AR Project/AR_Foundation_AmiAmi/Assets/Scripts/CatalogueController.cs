ee80fc1974b7cf42dfd03420de258d3f95ce52bf
GoGitGood <54795662+GoGitGood@users.noreply.github.com>
2020-06-21 21:59:50 +0800

    commit the URL

--- AmiAmi AR Project/AR_Foundation_AmiAmi/Assets/Scripts/CatalogueController.cs
+++ AmiAmi AR Project/AR_Foundation_AmiAmi/Assets/Scripts/CatalogueController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,7 +12,6 @@ public class CatalogueController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
