cd761e6e6cfd3d32b04948181689f75898968e1e
GoGitGood <54795662+GoGitGood@users.noreply.github.com>
2020-06-27 01:41:14 +0800

    Completed poses opening from side

--- AmiAmi AR Project/AR_Foundation_AmiAmi/Assets/Finalized_UIFunctions.cs
+++ AmiAmi AR Project/AR_Foundation_AmiAmi/Assets/Finalized_UIFunctions.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class Finalized_UIFunctions : MonoBehaviour
 {
+    [Header("Menu Pages")]
     public GameObject[] StaticUI;
     public GameObject ActiveUI;
 
-    public bool InARmode = false;
+    [Space(10)]
+    [Header("poses stuff")]
+    public RectTransform PoseCatalogue;
+    public Vector3 openPos = new Vector3(-480, 0, 0);
+    public Vector3 closedPos = new Vector3(480, 0, 0);
+    private Vector3 buttonVelocity = Vector3.zero;
+    private float smoothTime = 0.5f;
 
+    [Space(20)]
+    public bool InARmode = false;
+    public bool openPoses = false;  // Poses pages sliding out
+
+    
     private SelectionManager SM;
+
     
 
     // Start is called before the first frame update
@@ -17,19 +30,36 @@ public class Finalized_UIFunctions : MonoBehaviour
     {
         SM = FindObjectOfType<SelectionManager>();
 
-        // disable all UI pages
+        // disable all UI pages but the first page
         ActiveUI.SetActive(false);
         foreach (GameObject obj in StaticUI)
         {
             obj.SetActive(false);
         }
+        StaticUI[0].SetActive(true);
+        openPoses = false;
 
+        Debug.Log(PoseCatalogue.localPosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (openPoses == false && PoseCatalogue.localPosition != closedPos)
+        {
+            // close catalogue  
+            PoseCatalogue.localPosition = Vector3.SmoothDamp(PoseCatalogue.localPosition, closedPos, ref buttonVelocity, smoothTime);
+        }
+        else if (openPoses == true && PoseCatalogue.localPosition != openPos)
+        {
+            // open catalogue
+            PoseCatalogue.localPosition = Vector3.SmoothDamp(PoseCatalogue.localPosition, openPos, ref buttonVelocity, smoothTime);
+
+            if (SelectionManager.SelectedFigurine != null)
+            {
+                SM.LoadDetailPage(SM.figureindex);
+            }
+        }
     }
 
     public void SelectStaticUI(int ArrayIndex)
@@ -40,6 +70,7 @@ public class Finalized_UIFunctions : MonoBehaviour
         }
 
         StaticUI[ArrayIndex].SetActive(true);
+        openPoses = false;
     }
 
     public void EnterARMode()
@@ -53,10 +84,12 @@ public class Finalized_UIFunctions : MonoBehaviour
         }
 
         ActiveUI.SetActive(true);
+        openPoses = false;
     }
 
     public void ExitARMode(int staticUI_ArrayIndex)
     {
+        openPoses = false;
         InARmode = false;
         ActiveUI.SetActive(false);
 
@@ -71,5 +104,11 @@ public class Finalized_UIFunctions : MonoBehaviour
     public void OpenURLLink()
     {
         SM.OpenURL();
+        openPoses = false;
+    }
+
+    public void TogglePosesPage()
+    {
+        openPoses = !openPoses;
     }
 }
