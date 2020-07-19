6a049954232d0b6845cc533be50fc7d7797f0ee7
GoGitGood <54795662+GoGitGood@users.noreply.github.com>
2020-07-19 18:49:20 +0800

    XR Input Devices
    Testing Input Devices from XR packages

---
+++ AmiAmi AR Project/VR_Whiteboard/Assets/Scripts/Experimental/OculusTouch_InvertRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OculusTouch_InvertRotation : MonoBehaviour
+{
+    public Vector3 setRotation = new Vector3(30, -180, -20);
+
+    void Start()
+    {
+        gameObject.GetComponent<Transform>().rotation = Quaternion.Euler(setRotation);
+    }
+}
