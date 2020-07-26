be16ef5c0722579bf3e383af7f1b4039ff47c1b4
Kok Tai LIM <koktai.lim@digipen.edu>
2020-07-27 02:24:10 +0800

    Brush Settings Menu

---
+++ AmiAmi AR Project/VR_Whiteboard/Assets/Scripts/UI_BrushSizeSlider.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_BrushSizeSlider : MonoBehaviour
+{
+    public InputField BrushSizeInputField;
+    Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetBrush_SliderValue()
+    {
+        int newValue = int.Parse(BrushSizeInputField.text);
+
+        if (newValue > slider.maxValue)
+        {
+            slider.value = slider.maxValue;
+        }
+        else
+        {
+            slider.value = newValue;
+        }
+    }
+
+    public void UpdateBrushSize()
+    {
+        Debug.Log(slider.value);
+    }
+}
